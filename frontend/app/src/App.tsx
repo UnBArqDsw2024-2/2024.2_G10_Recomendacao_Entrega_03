7b8b79df912b9f13f3a76e068b59827067c3cfe6
Guilherme <70172633+GuilhermeB12@users.noreply.github.com>
2025-01-06 18:25:51 -0300

    Correção dos conflitos

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import { AvaliacaoBase } from './Decorator/components/AvaliacaoBaseDecorator';
-import { DecoratorAvaliacao } from './Decorator/components/decorator/DecoratorAvaliacao';
-import { DecoratorComentario } from './Decorator/components/decorator/DecoratorComentario';
-import { DecoratorImagem } from './Decorator/components/decorator/DecoratorImagem';
-import { DecoratorTags } from './Decorator/components/decorator/DecoratorTags';
-import { DecoratorVideo } from './Decorator/components/decorator/DecoratorVideo';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/home';
+import SignIn from './pages/sign-in/sign-in';
+import ObserverRestauranteFavorito from './components/observer/observerRestauranteFavorito';
+import RestauranteAvaliacaoContainer from './components/ContainerPresent/RestauranteAvaliacaoContainer'
+
 
 function App() {
-  const avaliacao = new AvaliacaoBase(4);
-  const decorator = new DecoratorAvaliacao(avaliacao);
-
-  const avaliacaoComImagem = new DecoratorImagem(decorator, 'https://imagem.com/foto.jpg');
-  const avaliacaoComComentario = new DecoratorComentario(avaliacaoComImagem, 'Muito bom!');
-  const avaliacaoComVideo = new DecoratorVideo(avaliacaoComComentario, 'https://video.com/video.mp4');
-  const avaliacaoComTags = new DecoratorTags(avaliacaoComVideo, ['Recomendo', 'Favorito']);
-
-  console.log(avaliacaoComTags.exibir());
-  avaliacaoComTags.arquivar();
-
   return (
-    <div>
-      <h1>Avaliação Decorada</h1>
-      <p>{avaliacaoComTags.exibir()}</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/observer" element={<ObserverRestauranteFavorito />} />
+        <Route path="/containerPresent" element={<RestauranteAvaliacaoContainer />} />
+      </Routes>
+    </Router>
   );
 }
 
